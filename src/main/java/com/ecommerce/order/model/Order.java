c2ad6fff312929bace600270b915de996e96dc59
vinoth kumar <corporatevinoth@gmail.com>
2023-06-15 00:21:46 +0530

    update model and service and add customValidation

--- src/main/java/com/ecommerce/order/model/Order.java
+++ src/main/java/com/ecommerce/order/model/Order.java
@@ -3,6 +3,8 @@ package com.ecommerce.order.model;
 import java.math.BigDecimal;
 import java.util.List;
 
+import com.ecommerce.order.customValidation.ValidPhoneNumber;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,7 +16,9 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,25 +29,47 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 @Entity
-@Table(name = "order_table")
+@Table(name = "orders")
 public class Order {
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long orderId;
+	
+	@NotNull
+	@Size(min = 20, message = "orderShippingAddress should have atleast 20 characters")
+	private String orderShippingAddress;
+	
 
+	@NotNull
+	@Size(min = 20, message = "orderBillingAddress should have atleast 20 characters")
+	private String orderBillingAddress;
+	
+	
 	@OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "pc_fid", referencedColumnName = "orderId")	
 	private List<Product> products;
 
 	@DecimalMin(value = "0.5", inclusive = false, message = "total price should be greater than 0.5 rs")
 	@Digits(integer = 3, fraction = 2)
-	private BigDecimal totalPrice;
+	private BigDecimal orderTotalPrice;
+	
+	
+	@DecimalMin(value = "0.5", inclusive = false, message = "tax price should be greater than 0.5 rs")
+	@Digits(integer = 3, fraction = 2)
+	private BigDecimal orderTax;
+	
+	@DecimalMin(value = "0.5", inclusive = false, message = "orderPrice should be greater than 0.5 rs")
+	@Digits(integer = 3, fraction = 2)
+	private BigDecimal orderPrice;
+	
+	@ValidPhoneNumber
+	private BigDecimal orderContactNo;
 
-	@Min(value = 1, message = "Product quantity should have atleast 2 characters")
+	@Min(value = 1, message = "Order quantity shoulde be greater than 1")
 	private Long quantity;
 
-	@Pattern(regexp = "^(NETBANKING|CARD|COD|WALLET|EMI)$", message = "invalid code")
+	@Pattern(regexp = "^(NETBANKING|CARD|COD|WALLET|EMI)$", message = "invalid payment type")
 	private String paymentType;
 	
 }
