ac213de9ff668d6f9ebaad085f024247448d7e35
vinoth kumar <corporatevinoth@gmail.com>
2023-06-22 13:04:10 +0530

    created master branch

--- src/main/java/com/ecommerce/customer/errorResponse/CustomerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.customer.errorResponse;
-
-public class CustomerNotFoundException extends RuntimeException {
-	   private static final long serialVersionUID = 1L;
-	   
-	  
-	}
